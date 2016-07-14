f3c7f263ca9849d4e70b17a5f38111d75649f738
kazumasa <toyboooxy@gmail.com>
2016-07-14 05:37:58 +0000

    made flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[success] = "初rails 実装いぇーい！"
       redirect_to @user 
     else
       render 'new'
